cb19053aba67378300e93d9f3ca352548342f0ba
JoseManuelTocino <josemanueltocinosierra@gmail.com>
2016-06-02 17:16:23 +0200

    Delete install.sh

--- onlinejob/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo Iniciando instalación... 
-read -p "Nombre del administrador de la base de datos:" usuario
-read -s -p "Contraseña del usuario:" contrasena
-mysql -u $usuario -p $contrasena < onlinejob.sql
-echo Instalación terminada.
-
-mvn tomcat7:run
